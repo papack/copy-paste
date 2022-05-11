6050042a81d46e6441aeb7c5e2cb3f800cf85f6f
Matthias Steiner <m@kjpjm.de>
2022-05-11 22:45:06 +0200

    add Locale Type

---
+++ TypeScript/types/Locale.ts
@@ -0,0 +1,102 @@
+export type Locale =
+  | "af" //Afrikaans
+  | "sq" //Albanisch
+  | "am" //Amharisch
+  | "ar" //Arabisch
+  | "hy" //Armenisch
+  | "as" //Assamesisch
+  | "az" //Aserbaidschanisch
+  | "bn" //Bengalisch
+  | "ba" //Baschkirisch
+  | "bs" //Bosnisch (Lateinisch)
+  | "bg" //Bulgarisch
+  | "yue" //Chinesisch (traditionell)
+  | "ca" //Katalanisch
+  | "lzh" //Chinesisch (literarisch)
+  | "zh-Hans" //Chinesisch (vereinfacht)
+  | "zh-Hant" //Chinesisch (traditionell)
+  | "hr" //Kroatisch
+  | "cs" //Tschechisch
+  | "da" //Dänisch
+  | "prs" //Dari
+  | "dv" //Divehi
+  | "nl" //Niederländisch
+  | "en" //Englisch
+  | "et" //Estnisch
+  | "fj" //Fidschi
+  | "fil" //Filipino
+  | "fi" //Finnisch
+  | "fr" //Französisch
+  | "fr-ca" //Französisch (Kanada)
+  | "ka" //Georgisch
+  | "de" //Deutsch
+  | "el" //Griechisch
+  | "gu" //Gujarati
+  | "ht" //Haitianisches Kreolisch
+  | "he" //Hebräisch
+  | "hi" //Hindi
+  | "mww" //Hmong Daw
+  | "hu" //Ungarisch
+  | "is" //Isländisch
+  | "id" //Indonesisch
+  | "iu" //Inuktitut
+  | "ga" //Irisch
+  | "it" //Italienisch
+  | "ja" //Japanisch
+  | "kn" //Kannada
+  | "kk" //Kasachisch
+  | "km" //Khmer
+  | "ko" //Koreanisch
+  | "ku" //Kurdisch (zentral)
+  | "kmr" //Kurdisch (nördlich)
+  | "ky" //Kirgisisch
+  | "lo" //Laotisch
+  | "lv" //Lettisch
+  | "lt" //Litauisch
+  | "mk" //Mazedonisch
+  | "mg" //Madagassisch
+  | "ms" //Malaiisch
+  | "ml" //Malayalam
+  | "mt" //Maltesisch
+  | "mi" //Maori
+  | "mr" //Marathi
+  | "mn-Cyrl" //Mongolisch (Kyrillisch)
+  | "mn-Mong" //Mongolisch (traditionell)
+  | "my" //Myanmar
+  | "ne" //Nepalesisch
+  | "nb" //Norwegisch
+  | "or" //Odia
+  | "ps" //Paschtu
+  | "fa" //Persisch
+  | "pl" //Polnisch
+  | "pt" //Portugiesisch (Brasilien)
+  | "pt-pt" //Portugiesisch (Portugal)
+  | "pa" //Pandschabi
+  | "otq" //Queretaro-Otomi
+  | "ro" //Rumänisch
+  | "ru" //Russisch
+  | "sm" //Samoanisch
+  | "sr-Cyrl" //Serbisch (Kyrillisch)
+  | "sr-Latn" //Serbisch (Lateinisch)
+  | "sk" //Slowakisch
+  | "sl" //Slowenisch
+  | "es" //Spanisch
+  | "sw" //Suaheli
+  | "sv" //Schwedisch
+  | "ty" //Tahitisch
+  | "ta" //Tamilisch
+  | "tt" //Tatarisch
+  | "te" //Telugu
+  | "th" //Thailändisch
+  | "bo" //Tibetisch
+  | "ti" //Tigrinya
+  | "to" //Tongaisch
+  | "tr" //Türkisch
+  | "tk" //Turkmenisch
+  | "uk" //Ukrainisch
+  | "ur" //Urdu
+  | "ug" //Uigurisch
+  | "uz" //Usbekisch (lateinisch
+  | "vi" //Vietnamesisch
+  | "cy" //Walisisch
+  | "yua"; //Yukatekisches Maya
